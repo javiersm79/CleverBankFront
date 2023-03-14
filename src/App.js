8990652b32eda899f305c2c6c8eaf40c9fca8c62
Javier Salazar <jsalazar@cleveritgroup.com>
2023-03-14 13:05:28 -0300

    Initial commit with initial react app created

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./assets/logo.png";
+import "./App.css";
+import Login from "./components/login/login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Clever Bank App
-        </p>
-        <p>
-          Clever Bank App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.email.value);
+
+    if (!e.target.email.value) {
+      alert("Email is required");
+    } else if (!e.target.email.value) {
+      alert("Valid email is required");
+    } else if (!e.target.password.value) {
+      alert("Password is required");
+    } else if (
+        e.target.email.value === "email@example.com" &&
+        e.target.password.value === "123456"
+    ) {
+      alert("Successfully logged in");
+      e.target.email.value = "";
+      e.target.password.value = "";
+    } else {
+      //setVisible(!isVisible)
+      alert("Wrong email or password combination");
+    }
+  };
+
+  handleClick = e => {
+    e.preventDefault();
+
+    alert("Goes to registration page");
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <img src={logo} className="logo" alt="Business view - Reports" />
+          <form className="form" onSubmit={this.handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" placeholder="your@email.com" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" />
+            </div>
+            <button className="primary">ENTRAR</button>
+          </form>
+          <Login/>
+
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/login/login.js
@@ -0,0 +1,10 @@
+import logo from "../../assets/logo.png";
+import React from "react";
+
+export default function Login() {
+    return (
+        <div>
+            <p className="error-msg">message to show in main app</p>
+        </div>
+    )
+}
